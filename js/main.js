cb612aa51d31e7307fee4efc2534eedbe729bbfd
smarenghi <smarenghi@gmail.com>
2015-06-27 00:27:55 -0300

    Version 0.5

--- js/main.js
+++ js/main.js
@@ -1,11 +1,16 @@
-////VALORES QUE VA A DEFINIR EL USUARIO
-////dimensiones en metros
-//var xSala = 20;	//largo de la sala (profundidad)
-//var ySala = 5; 		//alto de la sala
-//var zSala = 5;	//ancho de la sala
-//var xAsiento = 0.85;	//largo del asiento + espacio tras cada asiento (profundidad de filas). Default: 0.95
-//var zAsiento = 0.85;	//ancho del asiento. Buscar default
-////
+// //VALORES QUE VA A DEFINIR EL USUARIO
+// //dimensiones en metros
+// var xSala = 20;	//largo de la sala (profundidad)
+// var ySala = 5; 		//alto de la sala
+// var zSala = 14;	//ancho de la sala
+// var xAsiento = 0.85;	//largo del asiento + espacio tras cada asiento (profundidad de filas). Default: 0.95
+// var zAsiento = 0.85;	//ancho del asiento. Buscar default
+
+// var largoPantalla = 5;
+// var anchoPantalla = 5.74;
+// var distanciaPantallaPiso = 1.50; //1.50
+
+// var maxCalidad = true;
 
 function getDatos(){ //NO SE USA AL FINAL. BORRAR DESPUÉS
     var datos = document.getElementById("datos");
@@ -87,17 +92,16 @@ function invalidForm(){
     return resFinal;
 }
 
-//Las condiciones que se me ocurren:todos numeros, mayores que 0.
-//distanciaPantallaPiso >= 1.5
-//ySala > distanciaPantallaPiso + anchoPantalla + 0.2
-//largoPantalla < zSala
-//xAsiento < xSala
-//zAsiento < zSala
+//FUNCIONES
+//funcion para calcular la tangente en grados
+function getTanDeg(deg) {
+  var rad = deg * Math.PI/180;
+  return Math.tan(rad);
+}
 
 function isoptica(){
     
     if(!invalidForm()){ 
-    
     
     var datos = document.getElementById("datos");
     //largo de la sala (profundidad)
@@ -113,59 +117,59 @@ function isoptica(){
     var largoPantalla = Number(datos.largoPantalla.value);
     var anchoPantalla = Number(datos.anchoPantalla.value);
     var distanciaPantallaPiso =Number(datos.distanciaPantallaPiso.value);
-    
-    //otra opcion para dar el tamaño de la pantalla podría ser aspect ratio y diagonal
-    //var aspectRatio = 16/9; //que el menu despliegue las opcioes posibles
-    //var diagonalPantalla = 4 //diagonal de la pantalla ¿en metros? ¿pulgadas?
-    
-    
-    //PARAMETROS FIJOS
-    //se puede darle al usuario la posibilidad de elegir "máxima calidad" o "máxima cantidad de butacas"
-    var diferenciaAltura = 0.10; //Entre los ojos y el tope de la cabeza. ¿Optima 0.15?
-    var alturaOjosEspectador = 1.50; //de los pies a los ojos, sentado
-    var alfa = 61.8; //angulo en grados desde la mitad de la pantalla. Puede llegar hasta 80
 
-    //VALORES QUE PUEDE DETERMINAR LA CALCULADORA
-    var distanciaPrimerFila = 4.20;			//distancia de la primer fila a la pantalla. CALCULAR!!!
-    //var distanciaÚltimaFila = // De acuerdo al tema del ángulo
-    
-    //FUNCIONES
-    //funcion para calcular la tangente en grados
-    function getTanDeg(deg) {
-      var rad = deg * Math.PI/180;
-      return Math.tan(rad);
-    }
+	//PARAMETROS FIJOS
+	var diferenciaAltura = 0.12; //Entre los ojos y el tope de la cabeza. ¿Optima 0.15?
+	var alfa = 45;//30 a  45 //angulo en grados desde la mitad de la pantalla.
+	var beta = 80; //máximo angulo de visión horizontal
+	var gamma = 30; //mínimo ángulo de visión horizonral (varía de 26 a 36)
+	var alturaOjosEspectador = 1.50; //de los pies a los ojos, sentado
 
-        //CALCULO DE ISOPTICA
-        var xEscalon=[distanciaPrimerFila];
-        var yEscalon=[0];
-        var yOjos = [alturaOjosEspectador];
-        var distanciaFila = distanciaPrimerFila;
-        var yOjo = alturaOjosEspectador;
-        var tan = 0;
-        while (distanciaFila <= (xSala - xAsiento) && yOjo <= ySala - diferenciaAltura) {
-            tan = (distanciaPantallaPiso - yOjo - diferenciaAltura)/distanciaFila;
-            distanciaFila += xAsiento;
-            xEscalon.push(distanciaFila);
-            yOjo = distanciaPantallaPiso - tan * distanciaFila;
-            yOjos.push(yOjo);
-            yEscalon.push(yOjo - alturaOjosEspectador);
-        };
+	//VALORES QUE PUEDE DETERMINAR LA CALCULADORA
+	var distanciaPrimerFila = (largoPantalla/2)/getTanDeg(beta/2); //distancia de la primera fila a la pantalla
+	var distanciaÚltimaFila = (largoPantalla/2)/getTanDeg(gamma/2);//distancia máxima de la última fila a la pantalla, de acuerdo al ángulo de visión horizontal
+	//alert (distanciaÚltimaFila);
+	//alert (distanciaPrimerFila);
 
-        var cantidadFilas = xEscalon.length;
+	//CALCULO DE ISOPTICA
+	var xEscalon=[distanciaPrimerFila];
+	var yEscalon=[0];
+	var yOjos = [alturaOjosEspectador];
+	var distanciaFila = distanciaPrimerFila;
+	var yOjo = alturaOjosEspectador;
+	var tan = 0;
+	while (distanciaFila <= (xSala - xAsiento) && yOjo <= ySala - diferenciaAltura && distanciaFila+xAsiento<= distanciaÚltimaFila) {
+		tan = (distanciaPantallaPiso - yOjo - diferenciaAltura)/distanciaFila;
+		distanciaFila += xAsiento;
+		xEscalon.push(distanciaFila);
+		yOjo = distanciaPantallaPiso - tan * distanciaFila;
+		yOjos.push(yOjo);
+		yEscalon.push(yOjo - alturaOjosEspectador);
+	}
 
-        //CALCULO PARA LA PLANTA
-        var asientosPorFila = parseInt(zSala / zAsiento);	//en z
-        var centrarEnZ = (zSala % zAsiento)/2; //hay que saber este restito para poder centrar las filas
+	var cantidadFilas = xEscalon.length;
 
-        //valores para calcular la porcion quitada por angulo alfa (para dibujar las líneas rojas) FALTA
-        var zImpedidos = ( zSala / 2 );
-        var xImpedidos = zImpedidos * getTanDeg(90 -(alfa/2));
+	var radPrimer = Math.atan(anchoPantalla/distanciaPrimerFila);
+	var radUltimo = Math.atan(anchoPantalla/xEscalon[cantidadFilas-1]);
+	// alert(radPrimer*(180/Math.PI));
+	// alert(radUltimo*(180/Math.PI));
 
-        //DEVOLUCION DATOS
-        //mostrarlo como tabla
-        
-        var tablita = document.createElement("TABLE");
+	//CALCULO PARA LA PLANTA
+	var asientosPorFila = parseInt(zSala / zAsiento);	//en z
+	var centrarEnZ = (zSala % zAsiento)/2; //hay que saber este restito para poder centrar las filas
+
+	//valores para calcular la porcion quitada por angulo alfa (para dibujar las líneas rojas) FALTA
+	var zImpedidos = ( zSala / 2 );
+	var xImpedidos = zImpedidos * getTanDeg(90 -(alfa/2));
+
+	//DEVOLUCION DATOS
+	//mostrarlo como tabla
+	// document.getElementById("xEscalon").innerHTML = xEscalon;
+	// document.getElementById("yEscalon").innerHTML = yEscalon;
+	// document.getElementById("yOjos").innerHTML = yOjos;
+	// document.getElementById("cantidadFilas").innerHTML = cantidadFilas;
+	// document.getElementById("asientosPorFila").innerHTML = asientosPorFila;
+	var tablita = document.createElement("TABLE");
         
         var th = document.createElement("TH");
         var th2 = document.createElement("TH");
@@ -190,132 +194,127 @@ function isoptica(){
             celda1.innerHTML=Math.round((xEscalon[i]*100))/100;
             celda2.innerHTML=Math.round((yEscalon[i]*100))/100  ;
         }
-        
-        alert(xEscalon[0]);
-//        td2.appendChild(document.createTextNode(xEscalon[1]));
-        
-        
-//        document.getElementById("xEscalon").innerHTML = xEscalon;
         document.getElementById("xEscalon").appendChild(tablita);
-//        document.getElementById("yEscalon").innerHTML = yEscalon;
-//        document.getElementById("yOjos").innerHTML = yOjos;
         document.getElementById("cantidadFilas").innerHTML = cantidadFilas;
         document.getElementById("asientosPorFila").innerHTML = asientosPorFila;
-        document.getElementById("totalAsientos").innerHTML = cantidadFilas * asientosPorFila; //tener en cuenta que los ángulos te restan asientos!
+        document.getElementById("totalAsientos").innerHTML = cantidadFilas * asientosPorFila;
+	
+    //Calculo de asientos impedidos
+	// var impedidos = 0;
 
-        //CALCULO Y AJUSTE DE ESCALA
-        //se obtiene el elemento canvas del html, isoptico
-        var canvasiso = document.getElementById("canvasiso");
-        //se obtiene el elemento canvas del html, cenital
-        var canvascenital = document.getElementById("canvascenital");
+	// for (var i = 0; i < xEscalon; i++) {
+	// 	var impz = xEscalon[i]* getTanDeg(90 -(alfa/2))
+	// 	for (var j = 0; j < asientosPorFila; j++) {
+	// 		Things[i]
+	// 	};
+	// };
 
-        var canvasWidth = window.innerWidth *0.95; // Austar dibujos al ancho de la pantalla, con un pequeño margen.
-        var escala = (canvasWidth*0.95) / xSala;
-        var canvasIsoHeight = ySala * escala;
-        var canvasCenitalHeight = zSala * escala;
+	//CALCULO Y AJUSTE DE ESCALA
+	//se obtiene el elemento canvas del html, isoptico
+	var canvasiso = document.getElementById("canvasiso");
+	//se obtiene el elemento canvas del html, cenital
+	var canvascenital = document.getElementById("canvascenital");
 
-        canvasiso.width = canvasWidth;
-        canvasiso.height = canvasIsoHeight;
+	var canvasWidth = window.innerWidth *0.60; // Austar dibujos al ancho de la pantalla, con un pequeño margen.
+	var escala = (canvasWidth*0.95) / xSala;
+	var canvasIsoHeight = ySala * escala;
+	var canvasCenitalHeight = zSala * escala;
 
-        canvascenital.width = canvasWidth;
-        canvascenital.height = canvasCenitalHeight;
+	canvasiso.width = canvasWidth;
+	canvasiso.height = canvasIsoHeight;
 
+	canvascenital.width = canvasWidth;
+	canvascenital.height = canvasCenitalHeight;
 
-        //DIBUJO ISOPTICA
-        //se crea un objeto para dibujar
-        var ctxiso = canvasiso.getContext("2d");
+	//DIBUJO ISOPTICA
+	//se crea un objeto para dibujar
+	var ctxiso = canvasiso.getContext("2d");
 
-        //dibujan contorno y fondo de la sala de cine
-        ctxiso.fillStyle = "#FFFF00";
-        ctxiso.strokeStyle = 'black';
-        ctxiso.fillRect(0,0,canvasWidth,canvasIsoHeight);
-        ctxiso.strokeRect(0,0,canvasWidth,canvasIsoHeight);
+	//dibujan contorno y fondo de la sala de cine
+	ctxiso.fillStyle = "rgba(24, 188, 156, 0.35)";
+	ctxiso.strokeStyle = 'black';
+	ctxiso.fillRect(0,0,canvasWidth,canvasIsoHeight);
+	ctxiso.strokeRect(0,0,canvasWidth,canvasIsoHeight);
 
-        //dibuja la pantalla
-        ctxiso.fillStyle = "white";
-        //ctxiso.strokeStyle = 'white';
-        ctxiso.fillRect(0,(ySala-(anchoPantalla+distanciaPantallaPiso))*escala,0.1*escala,anchoPantalla*escala);
-        ctxiso.strokeRect(0,(ySala-(anchoPantalla+distanciaPantallaPiso))*escala,0.1*escala,anchoPantalla*escala);
+	//dibuja la pantalla
+	ctxiso.fillStyle = "white";
+	//ctxiso.strokeStyle = 'white';
+	ctxiso.fillRect(0,(ySala-(anchoPantalla+distanciaPantallaPiso))*escala,0.1*escala,anchoPantalla*escala);
+	ctxiso.strokeRect(0,(ySala-(anchoPantalla+distanciaPantallaPiso))*escala,0.1*escala,anchoPantalla*escala);
 
-        //eligen color de borde y relleno de los escalones
-        ctxiso.fillStyle = "#00FF00";
-        //ctxiso.strokeStyle = 'black';
+	//eligen color de borde y relleno de los escalones
+	ctxiso.fillStyle = "#E53C9C";
+	//ctxiso.strokeStyle = 'black';
 
-        //dibuja los escalones
-        for (j in xEscalon){
-            ctxiso.fillRect(xEscalon[j]*escala,(ySala-yEscalon[j])*escala,xAsiento*escala,yEscalon[j]*escala); //mejorable, metodo scale()
-            ctxiso.strokeRect(xEscalon[j]*escala,(ySala-yEscalon[j])*escala,xAsiento*escala,yEscalon[j]*escala); //mejorable
-        }
+	//dibuja los escalones
+	for (j in xEscalon){
+		ctxiso.fillRect(xEscalon[j]*escala,(ySala-yEscalon[j])*escala,xAsiento*escala,yEscalon[j]*escala); //mejorable, metodo scale()
+		ctxiso.strokeRect(xEscalon[j]*escala,(ySala-yEscalon[j])*escala,xAsiento*escala,yEscalon[j]*escala); //mejorable
+	}
 
-        //eligen color de borde y relleno de las cabezas
-        ctxiso.fillStyle = "#bbbbbb";
-        //ctxiso.strokeStyle = 'black';
+	//eligen color de borde y relleno de las cabezas
+	ctxiso.fillStyle = "#E53C9C";
+	//ctxiso.strokeStyle = 'black';
 
-        //dibuja las cabezas
-        for (j in xEscalon){
-            ctxiso.beginPath();
-            ctxiso.arc((xEscalon[j]+(xAsiento-diferenciaAltura))*escala,((ySala-yEscalon[j])-alturaOjosEspectador)*escala,diferenciaAltura*escala,0,2*Math.PI);
-            ctxiso.fill();
-            ctxiso.stroke();
-        }
+	//dibuja las cabezas
+	for (j in xEscalon){
+		ctxiso.beginPath();
+		ctxiso.arc((xEscalon[j]+(xAsiento-diferenciaAltura))*escala,((ySala-yEscalon[j])-alturaOjosEspectador)*escala,diferenciaAltura*escala,0,2*Math.PI);
+		ctxiso.fill();
+		ctxiso.stroke();
+	}
 
-        //DIBUJO VISTA CENITAL
-        //se crea un objeto para dibujar
-        var ctxcenital = canvascenital.getContext("2d");
+	//DIBUJO VISTA CENITAL
+	//se crea un objeto para dibujar
+	var ctxcenital = canvascenital.getContext("2d");
 
-        //dibujan contorno y fondo de la sala de cine
-        ctxcenital.fillStyle = "#FFFF00";
-        ctxcenital.strokeStyle = 'black';
-        ctxcenital.fillRect(0,0,canvasWidth,canvasCenitalHeight);
-        ctxcenital.strokeRect(0,0,canvasWidth,canvasCenitalHeight);
+	//dibujan contorno y fondo de la sala de cine
+	ctxcenital.fillStyle = "rgba(24, 188, 156, 0.35)";
+	ctxcenital.strokeStyle = 'black';
+	ctxcenital.fillRect(0,0,canvasWidth,canvasCenitalHeight);
+	ctxcenital.strokeRect(0,0,canvasWidth,canvasCenitalHeight);
 
-        //dibuja la pantalla
-        ctxcenital.fillStyle = "white";
-        //ctxiso.strokeStyle = 'white';
-        ctxcenital.fillRect(0,((zSala-largoPantalla)/2)*escala,0.1*escala,largoPantalla*escala);
-        ctxcenital.strokeRect(0,((zSala-largoPantalla)/2)*escala,0.1*escala,largoPantalla*escala);
+	//dibuja la pantalla
+	ctxcenital.fillStyle = "white";
+	//ctxiso.strokeStyle = 'white';
+	ctxcenital.fillRect(0,((zSala-largoPantalla)/2)*escala,0.1*escala,largoPantalla*escala);
+	ctxcenital.strokeRect(0,((zSala-largoPantalla)/2)*escala,0.1*escala,largoPantalla*escala);
 
-        //eligen color de borde y relleno de los asientos
-        ctxcenital.fillStyle = "#0000FF";
-        //ctxcenital.strokeStyle = 'black';
+	//eligen color de borde y relleno de los asientos
+	ctxcenital.fillStyle = "#173140";
+	//ctxcenital.strokeStyle = 'black';
 
-        //dibujar los asientos
-        for (j in xEscalon){
-            for (i = 1; i <= asientosPorFila; i++) {
-                ctxcenital.fillRect(xEscalon[j]*escala,(centrarEnZ+zAsiento*(i-1))*escala,xAsiento*escala,zAsiento*escala); //mejorable
-                ctxcenital.strokeRect(xEscalon[j]*escala,(centrarEnZ+zAsiento*(i-1))*escala,xAsiento*escala,zAsiento*escala); //mejorable
-            }
-        }
-        
-        // for (j = 0; j < asientosPorFila; j++){
-        // 	for (i = 0; i < cantidadFilas; i++) {
-        // 		xA = primerFila+i*xAsiento;	//coordenada en x de la esquina superior izquierda de cada asiento
-        // 		zA = j*zAsiento;			//coordenada en z de la esquina superior izquierda de cada asiento
-        // 		if (xA >= ( zSala/2 - zAsiento*j) * getTanDeg(90 -(alfa/2)) //se fija que quede a la derecha de las líneas rojas
-        // 			// && acá falta otra condición para que no aparezcan los del otro lado
-        // 			){
-        // 			//dibuja cada asiento
-        // 			ctx.fillRect(xA*escala,zA*escala,xAsiento*escala,zAsiento*escala); //mejorable
-        // 			ctx.strokeRect(xA*escala,zA*escala,xAsiento*escala,zAsiento*escala);
+	//dibujar los asientos
+	for (j in xEscalon){
+		for (i = 1; i <= asientosPorFila; i++) {
+			ctxcenital.fillRect(xEscalon[j]*escala,(centrarEnZ+zAsiento*(i-1))*escala,xAsiento*escala,zAsiento*escala); //mejorable
+			ctxcenital.strokeRect(xEscalon[j]*escala,(centrarEnZ+zAsiento*(i-1))*escala,xAsiento*escala,zAsiento*escala); //mejorable
+		}
+	}
+// for (j = 0; j < asientosPorFila; j++){
+// 	for (i = 0; i < cantidadFilas; i++) {
+// 		xA = primerFila+i*xAsiento;	//coordenada en x de la esquina superior izquierda de cada asiento
+// 		zA = j*zAsiento;			//coordenada en z de la esquina superior izquierda de cada asiento
+// 		if (xA >= ( zSala/2 - zAsiento*j) * getTanDeg(90 -(alfa/2)) //se fija que quede a la derecha de las líneas rojas
+// 			// && acá falta otra condición para que no aparezcan los del otro lado
+// 			){
+// 			//dibuja cada asiento
+// 			ctx.fillRect(xA*escala,zA*escala,xAsiento*escala,zAsiento*escala); //mejorable
+// 			ctx.strokeRect(xA*escala,zA*escala,xAsiento*escala,zAsiento*escala);
 
-        // 		}
+// 		}
+		
+// 	}
+// }
 
-        // 	}
-        // }
-
-        //dibuja las dos líneas rojas (a modo de guia, después las quitamos.)
-        ctxcenital.strokeStyle = 'red';
-        ctxcenital.moveTo(xImpedidos*escala,0);
-        ctxcenital.lineTo(0,zImpedidos*escala);
-        ctxcenital.stroke();
-        ctxcenital.moveTo(xImpedidos*escala,zSala*escala);
-        ctxcenital.lineTo(0,zImpedidos*escala);
-        ctxcenital.stroke();
-
-
-    }
+	//dibuja las dos líneas rojas (a modo de guia, después las quitamos.)
+	ctxcenital.strokeStyle = '#E53C9C';
+	ctxcenital.stroke
+	ctxcenital.moveTo(xImpedidos*escala,0);
+	ctxcenital.lineTo(0,zImpedidos*escala);
+	ctxcenital.stroke();
+	ctxcenital.moveTo(xImpedidos*escala,zSala*escala);
+	ctxcenital.lineTo(0,zImpedidos*escala);
+	ctxcenital.stroke();
+	}
 }
-
-    
-
-
